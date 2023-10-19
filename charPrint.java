c7171049ed1091c1f1ffa7bb04deb3d517e371cc
Prince <princesinghup50@gmail.com>
2023-10-19 12:28:02 +0530

    How to print char

---
+++ charPrint.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class charPrint{
+ public static void main(String[] args){
+ char ch;
+System.out.println("Please inter your char");
+Scanner obj =new Scanner(System.in);
+ch=obj.next().charAt(3);
+System.out.println(ch);
+ 
+ }
+}
