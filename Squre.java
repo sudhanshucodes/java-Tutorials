013bf6c9642fbfd2ef2f979e76b8ff7d0c42fac4
sudhanshucodes <sudhanshusinghmp50@gmail.com>
2023-10-28 13:18:41 +0530

    Area of square Program

---
+++ Squre.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Squre{
+public static void main(String[] args){
+ int a,squre;
+System.out.println("Enter side squre");
+  Scanner obj=new Scanner(System.in);
+    a=obj.nextInt();
+    squre=a*a;
+    System.out.println(squre);
+    
+ }
+}
