5962f96e0d5f0bcc803d6f4878da65fa60f4e407
sudhanshucodes <sudhanshusinghmp50@gmail.com>
2023-11-06 11:22:00 +0530

    Arrays

---
+++ Array.java
@@ -0,0 +1,13 @@
+class Alien{
+ public static void main(String[] args){
+  int a[]=new int[5];
+  a[0]=520;
+  a[1]=258;
+  a[2]=652;
+  a[3]=369;
+  a[4]=753;
+  for(int i=4; i<5;i--){
+   System.out.print(a[i]+" ");
+  }
+ }
+}
